cc6d9849b0da557a20f52fab6e8482e8dd5bc92a
ValentinGritsuk <marselkaru.com@gmail.com>
2025-02-03 01:11:43 +0300

    tests ready

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture()
+def driver():
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+    yield driver
+
+@pytest.fixture()
+def before_after():
+    print('Before test')
+    yield None
+    print('\nAfter test')
